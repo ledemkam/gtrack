5674ff103b07ae7ffbaa5eaf591faf46c7ce99d7
ledemkam <ledemkam@gmail.com>
2025-02-13 19:55:41 +0100

    chore(authentification): sign-set-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider,SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,12 +29,6 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>  
           {children}
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,18 @@ export default function Home() {
         <div className="flex items-center justify-center flex-col py-10 w-full">
            <div>
              <div className="flex flex-col">
-               <h1 className="text-4xl tablet:text-5xl font-bold text-center">Übernehmen Sie die Kontrolle<br/> über Ihre Finanzen</h1>
+               <h1 className="text-4xl md:text-5xl font-bold text-center">Übernehmen Sie die Kontrolle<br/> über Ihre Finanzen</h1>
                <p className="py-6 text-gray text-center">
                 suiver z vos bugget et depenseda <br/> en toute simplicite avec notre application intuitive !</p>
                 <div className="flex justify-center gap-2 items-center">
                    <Link
-                     href={""}
-                     className="btn btn-sm tablet:btn-md btn-outline btn-accent"
+                     href={"/sign-in"}
+                     className="btn btn-sm md:btn-md btn-outline btn-accent"
                    >sich einlogen</Link>
                   
                    <Link 
-                     href={""}
-                     className="btn btn-sm tablet:btn-md btn-accent"
+                     href={"/sign-up"}
+                     className="btn btn-sm mdt:btn-md btn-accent"
                    >sich registrieren</Link>
                 </div>
              </div>
