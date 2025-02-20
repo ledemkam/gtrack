c19eaa28621c9258b1e2f1812e0b312722b0bf22
ledemkam <ledemkam@gmail.com>
2025-02-20 13:53:54 +0100

    chore(updates): corrector

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -48,7 +48,7 @@ const Page = () => {
         setIsLoading(false);
       }
     } catch (error) {
-      console.error("Erreur lors de la récupération des données:", error);
+      console.error("Fehler beim Abrufen der Daten:", error);
     }
   };
 
